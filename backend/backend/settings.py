e68c9b35b4ad2169f86d2d9130d59e8249107b1f
Cheah Wei Heng <cheah9098@gmail.com>
2025-07-15 16:27:08 +0800

    Revert "Configure CORS for dev"
    This reverts commit c798ee6e64646df32ecaffa7f3ff94575b3e273c.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,13 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework_simplejwt.token_blacklist',
-    'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser' # To use custom user model 
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,8 +54,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ALLOW_ALL_ORIGINS = True # Allow all origins for CORS, Use for development only, change to specific origins in production
 
 ROOT_URLCONF = 'backend.urls'
 
